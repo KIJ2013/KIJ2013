8fc284d5b417f6bb1b449f973bbcca52a8f59e90
Iain MacDonald <IJMacD@gmail.com>
2012-03-26 22:01:39 +0100

    Added ability to view item contents

--- news.js
+++ news.js
@@ -1,5 +1,6 @@
 ﻿var rssURL = "http://15thdoverscouts.org.uk/news.rss";
 var db;
+var activity = 'list';
 
 function onLoad() {
     if(!window.openDatabase)
@@ -24,20 +25,28 @@ function createDatabase() {
 */
 function loadItems()
 {
+    if(activity != 'list')
+        return;
   db.readTransaction(function(tx){
-    tx.executeSql('SELECT * FROM `items` ORDER BY `date` DESC LIMIT 30', [], function(tx, result){
-        $('#news-list').empty();
+    tx.executeSql('SELECT guid,title FROM `items` ORDER BY `date` DESC LIMIT 30', [], function(tx, result){
         var len = result.rows.length;
         if(len)
         {
+            var list = $('<ul id="news-list" class="listview"></ul>');
             for(var i=0;i<len;i++)
             {
                 var row = result.rows.item(i);
-                $('<li><a href="#">'+row.title+'</a></li>').appendTo('#news-list');
+                var li = $('<li></li>');
+                var item = $('<a href="#" id="'+row.guid+'">'+row.title+'</a>');
+                item.data('guid', row.guid);
+                item.click(onClickNewsItem);
+                li.append(item);
+                list.append(li);
             }
+            $('#body').empty().append(list);
         }
         else
-            $('#news-list').append('<div>Loading News</div>');
+            $('#body').append('<div>Loading News</div>');
     });
   });
 }
@@ -58,5 +67,63 @@ function fetchItems()
             });
         });
       loadItems();
-    },"xml").error(function(jqXHR,status,error){alert('Error Fetching Items: '+status);});
+    },"xml").error(function(jqXHR,status,error){showError('Error Fetching Items: '+status);});
+}
+
+function onClickNewsItem(event)
+{
+    var sender = $(event.target);
+    displayNewsItem(sender.data('guid'));
+}
+
+function displayNewsList()
+{
+    setActionBarUp('index.html');
+    setActionBarTitle('News');
+    activity = 'list';
+    loadItems();
+}
+
+function displayNewsItem(guid){
+    setActionBarUp(displayNewsList);
+    activity = 'item';
+    db.readTransaction(function(tx){
+        tx.executeSql('SELECT title,date,description FROM `items` WHERE guid = ? LIMIT 30', [guid], function(tx, result){
+            if(result.rows.length == 1)
+            {
+                var item = result.rows.item(0);
+                setActionBarTitle(item.title)
+                $('#body').empty();
+                var content = $('<div style="padding: 10px;"></div>');
+                content.append('<h1>'+item.title+'</h1>');
+                content.append(item.description);
+                $('#body').append(content);
+            }
+        });
+    });
+}
+
+function setActionBarUp(fn)
+{
+    if(typeof fn == "function")
+    {
+        $('#up-button').removeAttr('href');
+        $('#up-button').unbind();
+        $('#up-button').click(function(){fn();return false;});
+    }
+    else if(typeof fn == "string")
+    {
+        $('#up-button').unbind();
+        $('#up-button').attr('href', fn);
+    }
+}
+function setActionBarTitle(title)
+{
+    $('#action_bar h1').text(title);
+}
+function showError(message)
+{
+    $('#popup').text(message);
+    $('#popup').show();
+    setTimeout(function(){$('#popup').slideUp('normal')},5000);
 }

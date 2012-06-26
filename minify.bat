df8aa61bb18032172ab322c4f1283da37ef03cbb
Iain MacDonald <IJMacD@gmail.com>
2012-06-26 18:05:04 +0100

    Tidied up directory by separating images, styles and scripts into their own folders.

--- minify.bat
+++ minify.bat
@@ -1,10 +1,12 @@
 @echo off
 echo kij2013.js
-java -jar ..\yuicompressor-2.4.7\build\yuicompressor-2.4.7.jar -v -o kij2013.min.js kij2013.js
+java -jar ..\yuicompressor-2.4.7\build\yuicompressor-2.4.7.jar -v -o js/kij2013.min.js js/kij2013.js
 echo news.js
-java -jar ..\yuicompressor-2.4.7\build\yuicompressor-2.4.7.jar -v -o news.min.js news.js
+java -jar ..\yuicompressor-2.4.7\build\yuicompressor-2.4.7.jar -v -o js/news.min.js js/news.js
 echo events.js
-java -jar ..\yuicompressor-2.4.7\build\yuicompressor-2.4.7.jar -v -o events.min.js events.js
+java -jar ..\yuicompressor-2.4.7\build\yuicompressor-2.4.7.jar -v -o js/events.min.js js/events.js
+echo events.js
+java -jar ..\yuicompressor-2.4.7\build\yuicompressor-2.4.7.jar -v -o js/map.min.js js/map.js
 echo style.css
-java -jar ..\yuicompressor-2.4.7\build\yuicompressor-2.4.7.jar -v -o style.min.css style.css
+java -jar ..\yuicompressor-2.4.7\build\yuicompressor-2.4.7.jar -v -o css/style.min.css css/style.css
 pause

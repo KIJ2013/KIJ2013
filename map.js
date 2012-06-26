9962e1476fa9f5e0f81842f4249f76560cffd42c
Iain MacDonald <IJMacD@gmail.com>
2012-06-26 15:45:28 +0100

    Zoom levels 16-18 enabled and code simplified.

--- map.js
+++ map.js
@@ -4,29 +4,21 @@ KIJ2013.Map = function(){
         init: function(){
             KIJ2013.init();
             KIJ2013.showLoading();
-            var bounds = [0.5785846710205073, 51.299361979488744,
-                  0.5925965309143088, 51.305519711648124],
-                llBounds = new L.LatLngBounds(
-                      new L.LatLng(bounds[1],bounds[0]),
-                      new L.LatLng(bounds[3],bounds[2])),
-                minLon = bounds[0],
-                minLat = bounds[1],
-                maxLon = bounds[2],
-                maxLat = bounds[3],
-                layer = new L.ImageOverlay("map.png", llBounds);
-            KIJ2013.Map.map = new L.Map('body', {
-                zoomControl: false,
-                maxBounds: llBounds
+            var bounds = new L.LatLngBounds(
+                      new L.LatLng(51.299361979488744,0.5785846710205073),
+                      new L.LatLng(51.305519711648124,0.5925965309143088)),
+                layer = new L.ImageOverlay("map.png", bounds),
+                map = KIJ2013.Map.map = new L.Map('body', {
+                minZoom: 16,
+                maxZoom: 18,
+                maxBounds: bounds
             });
-            KIJ2013.Map.map.addLayer(layer);
-            KIJ2013.Map.map.setView(new L.LatLng(51.302,0.585),17);
-            KIJ2013.Map.map.locate();
-            KIJ2013.Map.map.on('locationfound', function(e) {
-                var lat = e.latlng.lat,
-                    lon = e.latlng.lon,
-                    radius = e.accuracy / 2;
-                if(lat > minLat && lat < maxLat && lon > minLon && lon < maxLon){
-
+            map.addLayer(layer);
+            map.setView(new L.LatLng(51.302,0.585),17);
+            map.locate();
+            map.on('locationfound', function(e) {
+                var radius = e.accuracy / 2;
+                if(bounds.contains(e.latlng)){
                     var marker = new L.Marker(e.latlng);
                     KIJ2013.Map.map.addLayer(marker);
                     marker.bindPopup("You are within " + radius + " meters from this point").openPopup();

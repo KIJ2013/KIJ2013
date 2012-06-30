422899825d14a60f64aada54124fd91ba7784e6d
Iain MacDonald <IJMacD@gmail.com>
2012-06-30 19:37:59 +0100

    Reworked to operate from single files.
    All javascript is now in kij2013.js and all html inside index.html
    
    Navigation reworked to fit with single file paradigm, including action bar
    up navigation. Pages also initialise more efficiently since being contained
    within one page. Titles for action bar and html are improved. Loading and
    popup behaviour improved.

--- js/kij2013.js
+++ js/kij2013.js
@@ -1,6 +1,8 @@
 var KIJ2013 = function(){
     var preferences = {},
-        TABLE_PREFERENCES = "preferences";
+        TABLE_PREFERENCES = "preferences",
+        loading,
+        popup;
     
     return {
         db: null,
@@ -31,6 +33,14 @@ var KIJ2013 = function(){
                     callback();
                 }
             });
+            $('#menu a').each(function(){
+                $(this).click(function(){KIJ2013.navigateTo($(this).text())});
+            });
+            KIJ2013.setActionBarUp();
+            $('section').hide();
+            $('#menu').show();
+            popup = $('#popup');
+            loading = $('#loading')
         },
         sql: function(sql, vars, callback){
             if(typeof callback == "function")
@@ -51,6 +61,13 @@ var KIJ2013 = function(){
             this.sql("INSERT OR REPLACE INTO " + TABLE_PREFERENCES +
                 "(key,value) VALUES (?,?)",[name,value]);
         },
+        navigateTo: function(name) {
+            $('section').hide();
+            $('#'+name.toLowerCase()).show();
+            if(KIJ2013[name] && typeof KIJ2013[name].init == "function")
+                KIJ2013[name].init();
+            setTimeout(function() {window.scrollTo(0, 1);}, 1);
+        },
         setActionBarUp: function(fn)
         {
             if(typeof fn == "function")
@@ -59,26 +76,46 @@ var KIJ2013 = function(){
                     fn();
                     return false;
                 });
+                $('#up-icon').css({'visibilty': 'normal'});
             }
             else if(typeof fn == "string")
             {
-                $('#up-button').unbind().attr('href', fn);
+                $('#up-button').unbind().click(function(){
+                    KIJ2013.navigateTo(fn);
+                    return false;
+                });
+                $('#up-icon').css('visibility', 'visible');
             }
+            else if(typeof fn == "undefined")
+                $('#up-icon').css('visibility', 'hidden');
         },
-        setActionBarTitle: function(title)
+        setTitle: function(title)
         {
-            $('#action_bar h1').text(title);
+            var blank = typeof title == "undefined" || title == "",
+                default_title = "KIJ2013";
+            $('title').text(blank ? default_title : default_title + " - " + title);
+            $('#action_bar h1').text(blank ? default_title : title);
         },
         showLoading: function()
         {
-            $('<div/>').attr('id', "loading")
-            .text("Loading").append(
-                $('<img/>').attr('src',"ajax-loader.gif")
-            ).appendTo('#body');
+            if(loading.length == 0)
+            {
+                loading = $('<div/>').attr('id', 'loading')
+                    .text("Loading").append(
+                        $('<img/>').attr('src',"img/ajax-loader.gif"))
+                    .appendTo('body');
+            }
+            loading.show();
+        },
+        hideLoading: function(){
+            loading.hide();
         },
         showError: function(message)
         {
-            var popup = $('#popup');
+            if(popup.length == 0)
+            {
+                popup = $('<div/>').attr('id', 'popup').appendTo('body');
+            }
             popup.text(message).show();
             setTimeout(function(){
                 popup.slideUp('normal')
@@ -86,3 +123,352 @@ var KIJ2013 = function(){
         }
     }
 }();
+KIJ2013.Menu = function(){
+    return {
+        init: function(){
+            KIJ2013.setActionBarUp();
+            KIJ2013.setTitle();
+            setTimeout(function(){window.scrollTo(0,1)},1);
+        }
+    }
+}();
+KIJ2013.News = function(){
+    //var rssURL = "http://www.kij13.org.uk/category/latest-news/feed/";
+    var rssURL = "news.rss",
+        TABLE_NAME = 'news',
+
+    createDatabase = function() {
+        KIJ2013.db.transaction(function(tx){
+            tx.executeSql('CREATE TABLE IF NOT EXISTS `' + TABLE_NAME +
+                '` (`guid` varchar(255) PRIMARY KEY, `title` varchar(255),' +
+                '`date` int, `description` text)');
+        });
+    },
+
+    /**
+    * Fetch new items from web
+    */
+    fetchItems = function()
+    {
+        $.get(rssURL, function(data){
+            KIJ2013.db.transaction(function(tx){
+                $(data).find('item').each(function(i,item){
+                    var guid = $(item).find('guid').text(),
+                        title = $(item).find('title').text(),
+                        date = new Date($(item).find('pubDate').text()),
+                        description = $(item).find('encoded').text();
+                    description = description || $(item).find('description').text();
+                    tx.executeSql('INSERT INTO `' + TABLE_NAME +
+                        '` (`guid`, `title`, `date`, `description`) VALUES (?, ?, ?, ?)',
+                        [guid, title, (date/1000), description]);
+                });
+            });
+            displayNewsList();
+        },"xml").error(function(jqXHR,status,error){
+            KIJ2013.showError('Error Fetching Items: '+status);
+        });
+    },
+
+    onClickNewsItem = function(event)
+    {
+        var sender = $(event.target);
+        displayNewsItem(sender.data('guid'));
+    },
+
+    displayNewsList = function()
+    {
+        KIJ2013.setActionBarUp('Menu');
+        KIJ2013.setTitle('News');
+        KIJ2013.db.readTransaction(function(tx){
+            tx.executeSql('SELECT guid,title FROM `' + TABLE_NAME + '` ORDER BY `date` DESC LIMIT 30', [], function(tx, result){
+                var len = result.rows.length,
+                    list = $('<ul/>').attr('id',"news-list").addClass("listview"),
+                    i=0;
+                if(len)
+                {
+                    for(;i<len;i++)
+                    {
+                        var row = result.rows.item(i),
+                            li = $('<li/>'),
+                            item = $('<a/>').attr('id', row.guid).text(row.title);
+                        item.data('guid', row.guid);
+                        item.click(onClickNewsItem);
+                        li.append(item);
+                        list.append(li);
+                    }
+                    $('#news').empty().append(list);
+                    KIJ2013.hideLoading();
+                }
+                else
+                    KIJ2013.showLoading();
+            });
+        });
+    },
+
+    displayNewsItem = function(guid){
+        KIJ2013.setActionBarUp(displayNewsList);
+        KIJ2013.db.readTransaction(function(tx){
+            tx.executeSql('SELECT title,date,description FROM `' + TABLE_NAME + '` WHERE guid = ? LIMIT 1', [guid], function(tx, result){
+                if(result.rows.length == 1)
+                {
+                    var item = result.rows.item(0),
+                        content = $('<div/>').css({"padding": "10px"});
+                    KIJ2013.setTitle(item.title);
+                    $('<h1/>').text(item.title).appendTo(content);
+                    content.append(item.description);
+                    $('#news').empty().append(content);
+                    setTimeout(function() {window.scrollTo(0, 1);}, 1);
+                }
+            });
+        });
+    };
+
+    return {
+        init: function(){
+            createDatabase();
+            displayNewsList();
+            fetchItems();
+        }
+    }
+}();
+KIJ2013.Events = function(){
+
+    /**
+     * PRIVATE Variables
+     */
+    //var rssURL = "http://www.kij13.org.uk/category/events/feed/";
+    var jsonURL = "events.json",
+        TABLE_NAME = "events",
+
+    /**
+     * Create Database
+     */
+    createDatabase = function () {
+        KIJ2013.sql('CREATE TABLE IF NOT EXISTS `' + TABLE_NAME +
+            '` (`guid` varchar(255) PRIMARY KEY,`title` varchar(255),' +
+            '`date` int,`category` varchar(255),`remind` bool,`description` text)');
+    },
+
+    /**
+    * Fetch new items from web
+    */
+    fetchItems = function()
+    {
+        $.get(jsonURL, function(data){
+            KIJ2013.db.transaction(function(tx){
+                $(data).each(function(i,item){
+                    tx.executeSql('INSERT INTO `' + TABLE_NAME +
+                        '` (`guid`,`title`,`date`,`category`,`remind`,' +
+                        '`description`) VALUES (?, ?, ?, ?, ?, ?)', [item.guid,
+                        item.title, item.date, item.category, item.remind?1:0,
+                        item.description]);
+                });
+            });
+            displayEventsList();
+        },"json").error(function(jqXHR,status,error){
+            KIJ2013.showError('Error Fetching Events: '+status);
+        });
+    },
+
+    onClickEventItem = function()
+    {
+        displayEvent($(this).data('guid'));
+    },
+
+    onClickRemind = function(event)
+    {
+        var guid = event.data.guid,
+            className = "selected",
+            remind = $(this).toggleClass(className).hasClass(className);
+        KIJ2013.sql('UPDATE ' + TABLE_NAME + ' SET `remind` = ? ' +
+                'WHERE `guid` = ?', [remind?1:0, guid]);
+        return false;
+    },
+
+    displayEventsList = function()
+    {
+        KIJ2013.setActionBarUp('Menu');
+        KIJ2013.setTitle('Events');
+        var subcamp = KIJ2013.getPreference('subcamp');
+        KIJ2013.sql('SELECT guid,title,date,category,remind FROM `' + TABLE_NAME +
+                '` WHERE `date` > ? AND (`category` = ? OR `category` = "all") ' +
+                'ORDER BY `date` ASC LIMIT 30',
+                [(new Date())/1000,subcamp], function(tx, result){
+            var len = result.rows.length,
+                list = $('<ul/>').attr('id', "event-list").addClass("listview"),
+                month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                i=0;
+            if(len)
+            {
+                for(;i<len;i++)
+                {
+                    var row = result.rows.item(i),
+                        guid = row.guid,
+                        li = $('<li/>'),
+                        item = $('<a/>').attr('id', guid),
+                        date = new Date(row.date*1000),
+                        datetext = $('<p/>')
+                        .addClass('date-text')
+                        .text(date.getDate() + " " + month[date.getMonth()]),
+                        text = $('<p/>')
+                        .addClass('title')
+                        .text(row.title),
+                        remind = $('<a/>')
+                        .addClass('remind-btn button')
+                        .addClass(row.remind ? 'selected' : '')
+                        .text('Remind')
+                        .click({guid:guid},onClickRemind),
+                        category = $('<p/>')
+                        .addClass('category')
+                        .text(row.category);
+                    item.data('guid', row.guid);
+                    item.click(onClickEventItem);
+                    item.append(datetext).append(text)
+                        .append(remind).append(category);
+                    li.append(item);
+                    list.append(li);
+                }
+                $('#events').empty().append(list);
+                KIJ2013.hideLoading();
+            }
+            else
+                KIJ2013.showLoading();
+        });
+    },
+
+    displayEvent = function(guid){
+        KIJ2013.setActionBarUp(displayEventsList);
+        var subcamp = KIJ2013.getPreference('subcamp');
+        KIJ2013.sql('SELECT title,date,remind,category,description FROM `' +
+                TABLE_NAME + '` WHERE guid = ? AND ' +
+                '(`category` = ? OR `category` = "all") LIMIT 1', [guid,subcamp],
+                function(tx, result){
+            if(result.rows.length == 1)
+            {
+                var item = result.rows.item(0),
+                    content = $('<div/>').css({"padding": "10px"}),
+                    date = new Date(item.date*1000);
+                KIJ2013.setTitle(item.title)
+                $('#events').empty();
+                $('<h1/>').text(item.title).appendTo(content);
+                $('<p/>').addClass("date-text")
+                    .text(date.toLocaleString())
+                    .appendTo(content);
+                $('<a/>')
+                    .addClass('button')
+                    .addClass(item.remind ? 'selected' : '')
+                    .text('Remind')
+                    .click({guid:guid},onClickRemind)
+                    .appendTo(content);
+                $('<p/>')
+                    .attr('id', "remind-text")
+                    .text(item.remind?
+                        "You will be reminded about this event.":
+                        "You will not be reminded about this event")
+                    .appendTo(content);
+                $('<p/>')
+                    .addClass('category')
+                    .text(item.category)
+                    .appendTo(content);
+                $('<p/>')
+                    .text(item.description)
+                    .appendTo(content);
+                $('#events').append(content);
+                setTimeout(function() {window.scrollTo(0, 1);}, 1);
+            }
+        });
+    };
+
+    return {
+        init: function() {
+            createDatabase();
+            displayEventsList();
+            fetchItems();
+        }
+    }
+}();
+KIJ2013.Map = function(){
+    var img_bounds = [0.5785846710205073,51.299361979488744,0.5925965309143088,
+        51.305519711648124],
+        img_size = [2516,1867],
+        xScale = img_size[0]/(img_bounds[2]-img_bounds[0]),
+        yScale = img_size[1]/(img_bounds[3]-img_bounds[1]),
+        lonToX = function(lon){
+            if(lon<img_bounds[0])
+                return 0;
+            if(lon>img_bounds[2])
+                return img_size[0];
+            return (lon-img_bounds[0])*xScale;
+        },
+        latToY = function(lat){
+            if(lat<img_bounds[1])
+                return 0;
+            if(lat>img_bounds[3])
+                return img_size[1];
+            return (lat-img_bounds[1])*yScale;
+        },
+        marker,
+        initialised=false;
+    return {
+        init: function(){
+            if(!initialised){
+                marker = $('#marker');
+                initialised = false;
+            }
+            KIJ2013.setTitle('Map');
+            KIJ2013.setActionBarUp('Menu');
+            setTimeout(function(){KIJ2013.Map.moveTo(51.3, 0.585)},1);
+            KIJ2013.Map.mark(51.3, 0.585);
+        },
+        moveTo: function(lat, lon)
+        {
+            window.scrollTo(lonToX(lon), latToY(lat));
+        },
+        mark: function(lat, lon)
+        {
+            marker.css({display: 'block', left: lonToX(lon), top: latToY(lat)});
+        },
+        unmark: function(){
+            marker.css({display: 'none'});
+        }
+    }
+}();
+KIJ2013.Debug = function(){
+
+    /**
+     * PRIVATE Variables
+     */
+    var TABLE_NEWS = "news",
+        TABLE_EVENTS = "events",
+        initialised = false;
+
+    return {
+        init: function() {
+            if(!initialised){
+                $('#subcamp').val(KIJ2013.getPreference('subcamp'));
+                $('#clear-news').click(function(){
+                    KIJ2013.db.transaction(function(tx){
+                        tx.executeSql('DELETE FROM ' + TABLE_NEWS);
+                        alert("News Items Cleared");
+                    });
+                });
+                $('#clear-events').click(function(){
+                    KIJ2013.db.transaction(function(tx){
+                        tx.executeSql('DELETE FROM ' + TABLE_EVENTS);
+                        alert("Events Cleared");
+                    });
+                });
+                $('#set-subcamp').click(function(){
+                    var val = $('#subcamp').val();
+                    KIJ2013.setPreference("subcamp", val);
+                    alert("'subcamp' set to '" + val + "'");
+                });
+                initialised = true;
+            }
+            KIJ2013.setTitle('Debug');
+            KIJ2013.setActionBarUp('Menu')
+        }
+    }
+}();
+KIJ2013.init();
--- minify.bat
+++ minify.bat
@@ -1,12 +1,6 @@
 @echo off
 echo kij2013.js
 java -jar ..\yuicompressor-2.4.7\build\yuicompressor-2.4.7.jar -v -o js/kij2013.min.js js/kij2013.js
-echo news.js
-java -jar ..\yuicompressor-2.4.7\build\yuicompressor-2.4.7.jar -v -o js/news.min.js js/news.js
-echo events.js
-java -jar ..\yuicompressor-2.4.7\build\yuicompressor-2.4.7.jar -v -o js/events.min.js js/events.js
-echo map.js
-java -jar ..\yuicompressor-2.4.7\build\yuicompressor-2.4.7.jar -v -o js/map.min.js js/map.js
 echo style.css
 java -jar ..\yuicompressor-2.4.7\build\yuicompressor-2.4.7.jar -v -o css/style.min.css css/style.css
 pause

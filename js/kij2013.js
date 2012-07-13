aa8a1678158c8efeb8c58c8860b37d643d7e538f
Iain MacDonald <IJMacD@gmail.com>
2012-07-13 14:52:00 +0100

    Implemented lawnchair for preferences

--- js/kij2013.js
+++ js/kij2013.js
@@ -1,39 +1,26 @@
 var KIJ2013 = function(){
-    var preferences = {},
+    var preferences = {key:"preferences"},
         TABLE_PREFERENCES = "preferences",
         loading,
         beingLoaded,
-        popup;
+        popup,
+        store;
 
     return {
-        db: null,
         /**
          * Initialise KIJ2013 objects, databases and preferences
          * @param callback Allows a callack to be attached which fires when
          *   preferences have finished loading.
          */
         init: function(callback){
-            if(!window.openDatabase)
-            {
-                $('#body').html('<p>Sorry Support for your device is not ready yet. ' +
-                  'Please try again in the future.</p>');
-                return;
-            }
-            this.db = window.openDatabase("KIJ2013", "1.0", "KIJ2013 Database",
-              256*1024);
-            this.sql('CREATE TABLE IF NOT EXISTS `' + TABLE_PREFERENCES +
-              '` (`key` varchar(255) PRIMARY KEY,`value` varchar(255))');
-            this.sql("SELECT key,value FROM " + TABLE_PREFERENCES, [],
-              function(tx,results){
-                  var i, item;
-                for(i=0;i<results.rows.length;i++)
-                {
-                    item = results.rows.item(i);
-                    preferences[item.key] = item.value;
-                }
-                if(typeof callback == "function"){
-                    callback();
-                }
+            store = Lawnchair({name: TABLE_PREFERENCES}, function(){
+                this.get("preferences", function(pref){
+                    if(pref)
+                        preferences = pref;
+                    if(typeof callback == "function"){
+                        callback();
+                    }
+                });
             });
             $('#menu a').each(function(){
                 $(this).click(function(){KIJ2013.navigateTo($(this).text())});
@@ -44,24 +31,14 @@ var KIJ2013 = function(){
             popup = $('#popup');
             loading = $('#loading')
         },
-        sql: function(sql, vars, callback){
-            if(typeof callback == "function")
-                this.db.readTransaction(function(tx){
-                    tx.executeSql(sql,vars,callback);
-                });
-            else
-                this.db.transaction(function(tx){
-                    tx.executeSql(sql,vars);
-                });
-        },
-        getPreference: function(name){
-            return preferences[name];
+        getPreference: function(name, def){
+            return preferences[name] || def || null;
         },
         setPreference: function(name, value)
         {
+            if(name == "key") return;
             preferences[name] = value;
-            this.sql("INSERT OR REPLACE INTO " + TABLE_PREFERENCES +
-                "(key,value) VALUES (?,?)",[name,value]);
+            store.save(preferences);
         },
         navigateTo: function(name) {
             var sections = $('section:visible'),

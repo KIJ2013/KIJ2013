efd21fd5c4a7e69919e97e3d20ac8afdd8e29e8a
Iain MacDonald <IJMacD@gmail.com>
2012-07-17 14:56:58 +0100

    Preload Loading image for responsiveness.

--- minify.bat
+++ minify.bat
@@ -1,8 +1,6 @@
 @echo off
 echo kij2013.js
 java -jar ..\yuicompressor-2.4.7\build\yuicompressor-2.4.7.jar -v -o js/kij2013.min.js js/kij2013.js
-echo kij2013.js
-java -jar ..\yuicompressor-2.4.7\build\yuicompressor-2.4.7.jar -v -o js/qrcode.min.js js/qrcode.js
 echo style.css
 java -jar ..\yuicompressor-2.4.7\build\yuicompressor-2.4.7.jar -v -o css/style.min.css css/style.css
 pause

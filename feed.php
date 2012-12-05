063646b4c9cb14c4a93df1887001a32d0bb9fa4b
Iain MacDonald <IJMacD@gmail.com>
2012-12-05 18:34:48 +0000

    Feed proxy updates
    Fix php open tag for strict server
    Check existance of f paramater for server with warnings
    Correct charset decleration
    Convert spaces to tabs

--- feed.php
+++ feed.php
@@ -1,15 +1,15 @@
 <?php
 $feeds = array(
-    'news.rss' => array(
-        'name' => "Kent International Jamboree 2013 &#187; Latest news",
-        'content-type' => "application/rss+xml",
-        'url' => "http://www.kij13.org.uk/category/latest-news/feed/")
+	'news.rss' => array(
+		'name' => "Kent International Jamboree 2013 &#187; Latest news",
+		'content-type' => "application/rss+xml",
+		'url' => "http://www.kij13.org.uk/category/latest-news/feed/")
 );
-if($_GET['f'] && isset($feeds[$_GET['f']]))
+if(isset($_GET['f']) && isset($feeds[$_GET['f']]))
 {
-    $feed = $feeds[$_GET['f']];
-    header("Content-Type:" . $feed['content-type']);
-    echo file_get_contents($feed['url']);
+	$feed = $feeds[$_GET['f']];
+	header("Content-Type:" . $feed['content-type']."; charset=utf-8");
+	echo file_get_contents($feed['url']);
 }
 else
 {
@@ -17,9 +17,9 @@ else
 <h1>KIJ2013 Feed Proxy</h1>
 <p>This proxy is designed for local circumvention of cross origin requests</p>
 <ul>
-<?
-    foreach ($feeds as $key => $feed)
-    {
-        echo '<li><a href="?f=' . $key . '">' . $feed['name'];
-    }
+<?php
+	foreach ($feeds as $key => $feed)
+	{
+		echo '<li><a href="?f=' . $key . '">' . $feed['name'];
+	}
 }

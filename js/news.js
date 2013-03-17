b397b62d014465b495d3759489cce7c923912fd8
Iain MacDonald <IJMacD@gmail.com>
2013-03-17 17:47:06 +0000

    Keep internal cache of items rather than always refetching from the database.

--- js/news.js
+++ js/news.js
@@ -6,10 +6,26 @@
         view = null,
         settings = {},
         events = $({}),
+        // Internal cache of loaded items
+        items = {},
+        currentItem,
 
     init = function(){
         settings = KIJ2013.getModuleSettings('News');
+        events.bind('itemsready', function(){
+            console.log('itemsready');
+            if(view == "item")
+                displayNewsItem(currentItem);
+            else
+                displayNewsList();
+            if(!contentready) {
+                events.trigger('contentready');
+                contentready = true;
+            }
+        });
+
         createDatabase();
+        loadItems();
         fetchItems();
     },
 
@@ -23,12 +39,14 @@
     */
     fetchItems = function()
     {
+        // Only fetch once at a time
         if(!fetching){
             fetching = true;
             $.get(settings.rssURL, function(data){
-                var items = [],
+                var new_items = [],
                     item,
                     imgs;
+                items = {};
                 $(data).find('item').each(function(i,xitem){
                     item = { key: $(xitem).find('guid').text(),
                             title: $(xitem).find('title').text(),
@@ -40,16 +58,11 @@
                     imgs = $(item.description).find('img');
                     if(imgs.length)
                         item.image = $(imgs[0]).attr('src');
-                    items.push(item);
+                    new_items.push(item);
+                    items[item.key] = item;
                 });
-                store.batch(items, function(){
-                    if(view == "list")
-                        displayNewsList();
-                    if(!contentready) {
-                        events.trigger('contentready');
-                        contentready = true;
-                    }
-                });
+                events.trigger('itemsready');
+                store.batch(new_items);
                 fetching = false;
             },"xml").error(function(jqXHR,status,error){
                 KIJ2013.showError('Error Fetching Items: '+status);
@@ -58,10 +71,28 @@
         }
     },
 
+    /**
+     * Load Items from Database
+     */
+    loadItems = function(){
+        store.all(function(dbitems){
+            if(dbitems.length)
+            {
+                dbitems.sort(KIJ2013.Util.sort('date', false));
+                items = {};
+                $.each(dbitems, function(index, item){
+                    items[item.key] = item;
+                });
+                events.trigger('itemsready');
+            }
+        });
+    },
+
     onClickNewsItem = function(event)
     {
         var sender = $(event.currentTarget);
-        displayNewsItem(sender.data('guid'));
+        currentItem = sender.data('guid');
+        displayNewsItem(currentItem);
     },
 
     displayNewsList = function()
@@ -70,37 +101,27 @@
         KIJ2013.setActionBarUp();
         KIJ2013.setTitle('News');
         KIJ2013.scrollTop();
-        store.all(function(items){
-            if(items.length)
-            {
-                items.sort(KIJ2013.Util.sort('date', false));
-                var list = $('<ul/>').attr('id',"news-list").addClass("listview");
-                $.each(items,function(index,item){
-                    var li, el, sp;
-                    li = $('<li/>');
-                    el = $('<a/>').attr('id', item.key);
-                    sp = $('<span/>').text(item.title);
-                    el.append(sp);
-                    el.data('guid', item.key);
-                    el.click(onClickNewsItem);
-                    if(index == 0){
-                        li.css({width: "100%"})
-                        el.css({height: "140px"});
-                    }
-                    if(item.image)
-                        el.css({backgroundImage: "url("+item.image+")"});
-                    el.css({backgroundColor: generateBackgroundColor(item.key)});
-                    li.append(el);
-                    list.append(li);
-                });
-                $('#news').empty().append(list);
-                KIJ2013.hideLoading();
-                if(!contentready) {
-                    events.trigger('contentready');
-                    contentready = true;
-                }
+        var list = $('<ul/>').attr('id',"news-list").addClass("listview");
+        $.each(items,function(index,item){
+            var li, el, sp;
+            li = $('<li/>');
+            el = $('<a/>').attr('id', item.key);
+            sp = $('<span/>').text(item.title);
+            el.append(sp);
+            el.data('guid', item.key);
+            el.click(onClickNewsItem);
+            if(index == 0){
+                li.css({width: "100%"})
+                el.css({height: "140px"});
             }
+            if(item.image)
+                el.css({backgroundImage: "url("+item.image+")"});
+            el.css({backgroundColor: generateBackgroundColor(item.key)});
+            li.append(el);
+            list.append(li);
         });
+        $('#news').empty().append(list);
+        KIJ2013.hideLoading();
     },
 
     displayNewsItem = function(guid){
@@ -108,14 +129,13 @@
         KIJ2013.setActionBarUp(function(){
             displayNewsList();
         });
-        store.get(guid, function(item){
-            var content = $('<div/>').css({"padding": "10px"});
-            KIJ2013.setTitle(item.title);
-            $('<h1/>').text(item.title).appendTo(content);
-            content.append(item.description);
-            $('#news').empty().append(content);
-            KIJ2013.scrollTop();
-        });
+        var item = items[guid],
+            content = $('<div/>').css({"padding": "10px"});
+        KIJ2013.setTitle(item.title);
+        $('<h1/>').text(item.title).appendTo(content);
+        content.append(item.description);
+        $('#news').empty().append(content);
+        KIJ2013.scrollTop();
     },
 
     generateBackgroundColor = function(data){
